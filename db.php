dd64c5e9d0c0147bf339397352b212b1917c8900
eliudgichuki-star <81557287+eliudgichuki-star@users.noreply.github.com>
2021-05-07 11:53:31 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$servername="ec2-54-166-167-192.compute-1.amazonaws.com";
+$username="ftefyysaztiijg";
+$password="cada1cc04c842c493f5f75194f9ef5e6e463b27f9468b87aa755a1bcbf5da67f";
+$dbname="ddd2v9f3fokp3q";
+$db_con=mysqli_connect($servername,$username,$password,$dbname);
+?>
---
+++ index.php
@@ -0,0 +1,254 @@
+<?php
+	session_start();
+	// if($_SESSION['login_user']==null){
+	// 	header('location:home_page.php');
+	// }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+View Pet Details
+</title>
+</head>
+<body>
+
+		
+		<style>
+		* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+/* Style the header */
+.header {
+  padding: 40px;
+  background: #841b2d;
+  
+}
+
+/* Increase the font size of the h1 element */
+.header h1 {
+  font-size: 40px;
+  color:white;
+}
+/* Style the top navigation bar */
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+}
+
+/* Style the navigation bar links */
+.navbar a {
+  float: left;
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 20px;
+  text-decoration: none;
+}
+
+/* Right-aligned link */
+.navbar a.right {
+  float: right;
+}
+
+/* Change color on hover */
+.navbar a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+
+
+
+
+		table {
+			 border-collapse: collapse; 
+			 margin-left: 20%;
+			 margin-right: 20%;
+			}
+			tr/*:nth-child(3)*/ {
+			 border: solid thin;
+			}
+			
+	
+
+/* Footer */
+.footer {
+  padding: 20px;
+  text-align: center;
+  background: #841b2d;
+}	
+
+
+
+
+
+
+img {vertical-align: middle;}
+
+* {box-sizing:border-box}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 100%;
+  position: relative;
+  margin: auto;
+}
+
+/* Hide the images by default */
+.mySlides {
+  display: none;
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -22px;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4}
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4}
+  to {opacity: 1}
+}
+
+
+
+
+		</style>
+		
+		
+	</head>
+	<body>
+		
+  <h1 style="text-align:center;">Pet management system</h1>
+  
+
+
+
+
+</div>
+<h3 style="color:#841b2d;"><center>Pet Related Info</center></h3></td>
+<table cellpadding="20">
+<tr style="color:#841b2d;">
+
+<th align="center">Name</th>
+
+
+</tr>
+
+<?php
+include "db.php";
+
+$query=mysqli_query($db_con,"select* from users");
+
+while($row=mysqli_fetch_array($query))
+	{
+	?>
+	<tr style="color:#841b2d;">
+
+     
+      <td align="center"><?php echo $row['name'];?></td>
+	   
+
+      
+	   
+	   
+	 </tr>
+	<?php } 
+	?>
+	</table>
+	
+	<br>
+	<br>
+	<br>
+	<div class="footer">
+  <h2>2020 All Right Reserved</h2>
+</div>
+		
+		</body>
+			
+</html>
+		
+		
+	
+
+
+
+
