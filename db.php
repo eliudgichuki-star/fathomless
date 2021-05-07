304fe86119506c3becf8718babe5448902843ca5
eliudgichuki-star <81557287+eliudgichuki-star@users.noreply.github.com>
2021-05-07 12:50:27 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 $host="ec2-54-166-167-192.compute-1.amazonaws.com";
-$port        = "port = 5432";
+$port =" 5432";
 $dbname="ddd2v9f3fokp3q";
 $credentials = " user= ftefyysaztiijg  password=cada1cc04c842c493f5f75194f9ef5e6e463b27f9468b87aa755a1bcbf5da67f";
 
