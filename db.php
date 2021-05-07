799d91c8ebeb107eb421d4f87ce7c75e40cef74a
eliudgichuki-star <81557287+eliudgichuki-star@users.noreply.github.com>
2021-05-07 12:13:08 +0300

    Update db.php

--- db.php
+++ db.php
@@ -3,5 +3,6 @@ $servername="ec2-54-166-167-192.compute-1.amazonaws.com";
 $username="ftefyysaztiijg";
 $password="cada1cc04c842c493f5f75194f9ef5e6e463b27f9468b87aa755a1bcbf5da67f";
 $dbname="ddd2v9f3fokp3q";
-$db_con=mysqli_connect($servername,$username,$password,$dbname);
+$port ="5432";
+$db_con=mysqli_connect($servername,$username,$password,$dbname,$port);
 ?>
