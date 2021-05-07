f791f616c1c75b96d9553c7f1d27f3452b650ca5
eliudgichuki-star <81557287+eliudgichuki-star@users.noreply.github.com>
2021-05-07 12:40:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-
+<?php
+	session_start();
+	// if($_SESSION['login_user']==null){
+	// 	header('location:home_page.php');
+	// }
+?>
 
 <!DOCTYPE html>
 <html>
@@ -23,22 +28,8 @@ View Pet Details
 
 <h3 style="color:#841b2d;"><center>Pet Related Info</center></h3></td>
 
-
 <?php
-$host="ec2-54-166-167-192.compute-1.amazonaws.com";
- $port = "port = 5432";
-$dbname="ddd2v9f3fokp3q";
-$credentials = "user = ftefyysaztiijg  password=cada1cc04c842c493f5f75194f9ef5e6e463b27f9468b87aa755a1bcbf5da67f";
-
-
-$db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
-?>
-
+include "db.php";
 
 $query=mysqli_query($db,"select* from users");
 
