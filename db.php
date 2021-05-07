02efaf43d32976ee2dcc5f6a7b710e61881c6c87
eliudgichuki-star <81557287+eliudgichuki-star@users.noreply.github.com>
2021-05-07 12:41:45 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
-$host="ec2-54-166-167-192.compute-1.amazonaws.com";
- $port = "port = 5432";
+$servername="ec2-54-166-167-192.compute-1.amazonaws.com";
+$username="ftefyysaztiijg";
+$password="cada1cc04c842c493f5f75194f9ef5e6e463b27f9468b87aa755a1bcbf5da67f";
 $dbname="ddd2v9f3fokp3q";
-$credentials = "user = ftefyysaztiijg  password=cada1cc04c842c493f5f75194f9ef5e6e463b27f9468b87aa755a1bcbf5da67f";
+$db_con=mysqli_connect($servername,$username,$password,$dbname);
 
 
-$db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db) {
+
+   if(!$dbcon) {
       echo "Error : Unable to open database\n";
    } else {
       echo "Opened database successfully\n";
